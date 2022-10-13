1b9451876976f75b8e759a5e628b3e461a716551
Yurii <yourussianbrother@gmail.com>
2022-10-13 20:27:11 +0300

    mainVC

--- Rgba/ViewControllers/MainViewController.swift
+++ Rgba/ViewControllers/MainViewController.swift
@@ -12,11 +12,7 @@ protocol NewColorDelegate {
 }
 
 class MainViewController: UIViewController {
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-    
+        
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let settingsVC = segue.destination as? SettingsViewController else {return}
         settingsVC.updatedColor = view.backgroundColor
